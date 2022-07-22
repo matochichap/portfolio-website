f9c7ab2e201c643372096f578eb2d9635a245438
blueturtle <turtlesarentblue@gmail.com>
2022-07-22 12:16:55 +0800

    Change sqlite to postgres

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ app = Flask(__name__)
 Bootstrap(app)
 
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db' local access to projects.db
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///projects.db')  # heroku projects.db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
